dfd3d9c6360372d38457e042a89a9c03f2a0829d
Marc0sGabriel <mgabriel4747@gmail.com>
2023-10-19 20:57:00 -0300

    feat: import component footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { FooterComponent } from './interface/components/Footer';
 import { HeaderComponent } from './interface/components/Header';
 
 export default function Home() {
   return (
     <main>
       <HeaderComponent />
+      <FooterComponent />
     </main>
   );
 }
