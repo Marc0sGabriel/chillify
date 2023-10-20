3ba4ddfe603bf8acd7359c4befe2d23b6be1a783
Marc0sGabriel <mgabriel4747@gmail.com>
2023-10-20 15:01:36 -0300

    feat: playing message status

--- app/interface/components/MusicPlayer/index.tsx
+++ app/interface/components/MusicPlayer/index.tsx
@@ -7,6 +7,7 @@ import {
   BsPauseFill,
   BsFillSkipEndFill,
   BsSkipStartFill,
+  BsBroadcast,
 } from 'react-icons/bs';
 
 export function MusicPlayerComponent() {
@@ -17,64 +18,69 @@ export function MusicPlayerComponent() {
   }
 
   return (
-    <aside className="relative my-0 mx-auto bg-gray-500 flex rounded-xl p-3 max-w-md bg-opacity-30 backdrop-blur-sm">
-      <div className="w-40 h-40">
-        <Image
-          className="object-cover w-full h-full rounded-lg"
-          width={96}
-          height={144}
-          objectFit="cover"
-          unoptimized
-          src={
-            'https://lofigirl.com/wp-content/uploads/elementor/thumbs/summer-boy-qc2031oy0tv0ndmc3vjny71vdjsvm8tdvfrah4drc0.png'
-          }
-          alt={''}
-        />
-      </div>
+    <>
+      <span className="text-gray-500 left-10 top-24 absolute flex items-center gap-2 w-fit ">
+        <BsBroadcast /> Playing now
+      </span>
+      <aside className="relative my-0 mx-auto bg-gray-500 flex rounded-xl p-3 max-w-md bg-opacity-30 backdrop-blur-sm">
+        <div className="w-40 h-40">
+          <Image
+            className="object-cover w-full h-full rounded-lg"
+            width={96}
+            height={144}
+            objectFit="cover"
+            unoptimized
+            src={
+              'https://lofigirl.com/wp-content/uploads/elementor/thumbs/summer-boy-qc2031oy0tv0ndmc3vjny71vdjsvm8tdvfrah4drc0.png'
+            }
+            alt={''}
+          />
+        </div>
 
-      <section
-        aria-label="music controls and music info"
-        className="ml-3 w-full"
-      >
-        <header>
-          <h3 aria-label="music title" className="text-2xl font-semibold">
-            Always with me
-          </h3>
-
-          <div
-            className="my-2 font-mono text-gray-300"
-            aria-label="artist name"
-          >
-            <small>Youmi Kimura</small>
-          </div>
-        </header>
-
-        <div
-          aria-label="music time left"
-          className="flex justify-between items-center mb-1 font-mono text-gray-300"
+        <section
+          aria-label="music controls and music info"
+          className="ml-3 w-full"
         >
-          <small>00:25</small>
-          <small>3:45</small>
-        </div>
+          <header>
+            <h3 aria-label="music title" className="text-2xl font-semibold">
+              Always with me
+            </h3>
 
-        <div className="h-0.5 w-full bg-gray-400 rounded mb-5">
+            <div
+              className="my-2 font-mono text-gray-300"
+              aria-label="artist name"
+            >
+              <small>Youmi Kimura</small>
+            </div>
+          </header>
+
           <div
-            aria-label="music progress bar"
-            className="h-0.5 w-1/4  bg-orange-400 rounded"
-          />
-        </div>
+            aria-label="music time left"
+            className="flex justify-between items-center mb-1 font-mono text-gray-300"
+          >
+            <small>00:25</small>
+            <small>3:45</small>
+          </div>
 
-        <ul className="flex items-center gap-5 w-fit mx-auto my-0">
-          <BsSkipStartFill className="h-7 w-7 cursor-pointer" />
-          <BsPlayFill
-            aria-label="button to play the song"
-            className="h-9 w-9 cursor-pointer"
-            onClick={() => togglePlayPauseMusic()}
-          />
+          <div className="h-0.5 w-full bg-gray-400 rounded mb-5">
+            <div
+              aria-label="music progress bar"
+              className="h-0.5 w-1/4  bg-orange-400 rounded"
+            />
+          </div>
 
-          <BsFillSkipEndFill className="h-7 w-7 cursor-pointer" />
-        </ul>
-      </section>
-    </aside>
+          <ul className="flex items-center gap-5 w-fit mx-auto my-0">
+            <BsSkipStartFill className="h-7 w-7 cursor-pointer" />
+            <BsPlayFill
+              aria-label="button to play the song"
+              className="h-9 w-9 cursor-pointer"
+              onClick={() => togglePlayPauseMusic()}
+            />
+
+            <BsFillSkipEndFill className="h-7 w-7 cursor-pointer" />
+          </ul>
+        </section>
+      </aside>
+    </>
   );
 }
