bd841b7962b704f77d00aa919749e2cccc69f29e
Marc0sGabriel <mgabriel4747@gmail.com>
2023-10-20 13:18:42 -0300

    feat: update website title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Chillify',
+  title: 'Chillify - Lofi Music for Productivity',
   description: `Chillify is your go-to destination for a seamless productivity experience. Immerse yourself in the soothing world of lo-fi music, carefully curated to elevate your focus and boost your productivity.`,
 };
 
