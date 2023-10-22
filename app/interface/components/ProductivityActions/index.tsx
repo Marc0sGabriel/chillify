939f5ac40a2a32de05263d254398cbb81532b0be
Marc0sGabriel <mgabriel4747@gmail.com>
2023-10-22 15:32:04 -0300

    :sparkles: feat: show/hide pomodoro by click

--- app/interface/components/ProductivityActions/index.tsx
+++ app/interface/components/ProductivityActions/index.tsx
@@ -1,20 +1,35 @@
+'use client';
+
 import { MdOutlineWatchLater, MdOutlineStickyNote2 } from 'react-icons/md';
+import { PomodoroWidget } from '../PomodoroWidget';
+import { useState } from 'react';
 
 export function ProductivityComponent() {
-  return (
-    <div className="flex items-center gap-2">
-      <MdOutlineStickyNote2
-        className="w-5 h-5 hover:cursor-pointer"
-        aria-label="button to create a task"
-      />
+  const [showPomodoro, setShowPomodoro] = useState(false);
 
-      <button
-        className="tooltip tooltip-bottom hover:cursor-pointer"
-        data-tip="pomodoro"
-        aria-label="button to create a pomodoro"
-      >
-        <MdOutlineWatchLater className="w-5 h-5" />
-      </button>
-    </div>
+  function handlePomodoroDisplay() {
+    setShowPomodoro(!showPomodoro);
+  }
+
+  return (
+    <>
+      <div className="flex items-center gap-2 justify-center relative top-[-4rem]">
+        <MdOutlineStickyNote2
+          className="w-5 h-5 hover:cursor-pointer"
+          aria-label="button to create a task"
+        />
+
+        <button
+          onClick={() => handlePomodoroDisplay()}
+          className="tooltip tooltip-bottom hover:cursor-pointer"
+          data-tip="pomodoro"
+          aria-label="button to create a pomodoro"
+        >
+          <MdOutlineWatchLater className="w-5 h-5" />
+        </button>
+      </div>
+
+      {showPomodoro && <PomodoroWidget />}
+    </>
   );
 }
