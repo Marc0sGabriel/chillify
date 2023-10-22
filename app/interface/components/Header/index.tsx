d9d835f294325b5d77e8840e3f9f0cf5849de3d0
Marc0sGabriel <mgabriel4747@gmail.com>
2023-10-22 09:28:18 -0300

    :sparkles: feat: import Productivity component

--- app/interface/components/Header/index.tsx
+++ app/interface/components/Header/index.tsx
@@ -1,9 +1,12 @@
 import { SiGithub, SiGmail, SiLinkedin } from 'react-icons/si';
+import { ProductivityComponent } from '../ProductivityActions';
 
 export function HeaderComponent() {
   return (
     <header className="flex justify-between p-10">
       <h1 className="font-bold text-4xl">Chillify</h1>
+
+      <ProductivityComponent />
 
       <nav className="flex items-center gap-4">
         <a
