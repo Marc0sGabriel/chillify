96804a9be1f2671c1502687148045978a4690f37
Marc0sGabriel <mgabriel4747@gmail.com>
2023-10-22 09:29:07 -0300

    :lipstick: styles: hover and aria-label

--- app/interface/components/ProductivityActions/index.tsx
+++ app/interface/components/ProductivityActions/index.tsx
@@ -1,10 +1,17 @@
 import { MdOutlineWatchLater, MdOutlineStickyNote2 } from 'react-icons/md';
+import { PomodoroWidget } from '../PomodoroWidget';
 
 export function ProductivityComponent() {
   return (
     <div className="flex items-center gap-2">
-      <MdOutlineStickyNote2 className="w-5 h-5" />
-      <MdOutlineWatchLater className="w-5 h-5" />
+      <MdOutlineStickyNote2
+        className="w-5 h-5 hover:cursor-pointer"
+        aria-label="button to create a task"
+      />
+      <MdOutlineWatchLater
+        className="w-5 h-5 hover:cursor-pointer"
+        aria-label="button to create a pomodoro"
+      />
     </div>
   );
 }
