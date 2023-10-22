aee8ca71c1fffce8927298932ee7bcb3eb738cac
Marc0sGabriel <mgabriel4747@gmail.com>
2023-10-22 17:17:53 -0300

    :lipstick: styles: add grip icon

--- app/interface/components/PomodoroWidget/index.tsx
+++ app/interface/components/PomodoroWidget/index.tsx
@@ -1,16 +1,19 @@
 import { Montserrat } from 'next/font/google';
 import { HiOutlineArrowPath } from 'react-icons/hi2';
+import { BsGripVertical } from 'react-icons/bs';
+
 const montSerrat = Montserrat({ subsets: ['latin'] });
 
 export function PomodoroWidget() {
   return (
-    <>
+    <div>
       <nav className="w-fit mx-auto">
         <button className="btn btn-ghost btn-xs">Pomodoro</button>
         <button className="btn btn-ghost btn-xs">Short break</button>
         <button className="btn btn-ghost btn-xs">Long break</button>
       </nav>
-      <div className="card w-[24rem] p-3 bg-zinc-700 mx-auto mb-10 flex-row justify-between items-center gap-5">
+      <div className="card w-[24rem] p-3 bg-zinc-700 mx-auto mt-1 mb-10 flex-row justify-between items-center gap-5">
+        <BsGripVertical className="h-7 w-7" />
         <section className={`${montSerrat.className} text-5xl font-bold`}>
           <span>2</span>
           <span>5</span>
@@ -29,6 +32,6 @@ export function PomodoroWidget() {
           </button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
