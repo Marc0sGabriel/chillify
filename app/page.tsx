9dba60c9613c6aee7ed586ddc604e1b6025eab3b
Marc0sGabriel <mgabriel4747@gmail.com>
2023-10-22 15:11:37 -0300

    :sparkles: import: Pomodoro widget in main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { HeaderComponent } from './interface/components/Header';
 import { MusicPlayerComponent } from './interface/components/MusicPlayer';
+import { PomodoroWidget } from './interface/components/PomodoroWidget';
 
 export default function Home() {
   return (
     <main>
       <HeaderComponent />
+      <PomodoroWidget />
       <MusicPlayerComponent />
     </main>
   );
