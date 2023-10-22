972bbccf39fd3f295fa2e9c120743acc6fb9a3c9
Marc0sGabriel <mgabriel4747@gmail.com>
2023-10-22 09:22:39 -0300

    :sparkles: feat: pomodoro widget initial design

---
+++ app/interface/components/PomodoroWidget/index.tsx
@@ -0,0 +1,13 @@
+export function PomodoroWidget() {
+  return (
+    <div>
+      <section>
+        <span>2</span>
+        <span>5</span>
+        <span>:</span>
+        <span>0</span>
+        <span>0</span>
+      </section>
+    </div>
+  );
+}
