fc531696e8ec8846ed76f5699300201dd57d8182
Marc0sGabriel <mgabriel4747@gmail.com>
2023-10-24 15:44:54 -0300

    :sparkles: feat: is draggable widget todo-list

--- app/interface/components/TodoListWidget/index.tsx
+++ app/interface/components/TodoListWidget/index.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { BsXOctagon, BsGripHorizontal, BsPlusLg } from 'react-icons/bs';
-import { Tasks } from './Tasks';
+import { ChangeEvent, FormEvent, useRef, useState } from 'react';
 import { z } from 'zod';
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { Tasks } from './Tasks';
+import { BsXOctagon, BsGripHorizontal, BsPlusLg } from 'react-icons/bs';
+import Draggable from 'react-draggable';
 
 const taskSchema = z.object({
   getTaskValue: z
@@ -32,45 +33,46 @@ export function TodoListComponent() {
   }
 
   const totalTasks = task.length - 1;
-
   const handleErrorTaskLength = getTaskValue.length >= 40;
   const isEmptyTaskField = getTaskValue.length === 0;
 
   return (
-    <div className="card w-[30rem] bg-zinc-800 p-3 mx-auto mb-7">
-      <BsGripHorizontal className="h-7 w-7 self-end cursor-grab" />
-      <h3 className="text-2xl font-medium">Your tasks today</h3>
-      <span className="font-mono">0/{totalTasks}</span>
-      <div className="divider" />
+    <Draggable>
+      <div className="card w-[30rem] bg-zinc-800 p-3 mx-auto mb-7 absolute">
+        <BsGripHorizontal className="h-7 w-7 self-end cursor-grab" />
+        <h3 className="text-2xl font-medium">Your tasks today</h3>
+        <span className="font-mono">0/{totalTasks}</span>
+        <div className="divider" />
 
-      <form onSubmit={handleSubmit} action="" className="flex gap-4">
-        <input
-          type="text"
-          onChange={onChangeTasks}
-          required
-          placeholder="What's need be done?"
-          className="input w-full mb-5 bg-transparent input-bordered focus:outline-sky-400"
-        />
+        <form onSubmit={handleSubmit} action="" className="flex gap-4">
+          <input
+            type="text"
+            onChange={onChangeTasks}
+            required
+            placeholder="What's need be done?"
+            className="input w-full mb-5 bg-transparent input-bordered focus:outline-sky-400"
+          />
 
-        {/* Trocar por Ref para não renderizar atoa */}
-        {handleErrorTaskLength && (
-          <div className="animate-pulse alert alert-error w-96 fixed left-12 top-[45rem] font-mono font-bold">
-            <BsXOctagon className="w-5 h-5" /> Error: task name is too long!
-          </div>
-        )}
+          {/* Trocar por Ref para não renderizar atoa */}
+          {handleErrorTaskLength && (
+            <div className="animate-pulse alert alert-error w-96 fixed left-12 top-[45rem] font-mono font-bold">
+              <BsXOctagon className="w-5 h-5" /> Error: task name is too long!
+            </div>
+          )}
 
-        <button
-          disabled={isEmptyTaskField}
-          className="btn btn-circle btn-outline text-2xl border-zinc-200 hover:bg-zinc-200"
-        >
-          <BsPlusLg className="w-6 h-6" />
-        </button>
-      </form>
+          <button
+            disabled={isEmptyTaskField}
+            className="btn btn-circle btn-outline text-2xl border-zinc-200 hover:bg-zinc-200"
+          >
+            <BsPlusLg className="w-6 h-6" />
+          </button>
+        </form>
 
-      {/* TASK LIST */}
-      <ul className="max-h-52 overflow-y-auto px-2">
-        {task.map((task) => <Tasks key={task} taskName={task} />).reverse()}
-      </ul>
-    </div>
+        {/* TASK LIST */}
+        <ul className="max-h-52 overflow-y-auto px-2">
+          {task.map((task) => <Tasks key={task} taskName={task} />).reverse()}
+        </ul>
+      </div>
+    </Draggable>
   );
 }
