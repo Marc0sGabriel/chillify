35e7adf8244967e39d97903ee764dd5e6082f4a4
Marc0sGabriel <mgabriel4747@gmail.com>
2023-11-02 08:28:03 -0300

    :sparkles: feat: btn check tasks

--- app/interface/components/TodoListWidget/Tasks/index.tsx
+++ app/interface/components/TodoListWidget/Tasks/index.tsx
@@ -1,10 +1,23 @@
-import { BsCheckSquareFill, BsFillTrash3Fill } from 'react-icons/bs';
+import { Dispatch, SetStateAction } from 'react';
+import { BsCheckSquareFill, BsFillTrash3Fill, BsSquare } from 'react-icons/bs';
 
 interface TaskProps {
   taskName: string;
+  completedTask: boolean;
+  toggleTask: Dispatch<SetStateAction<boolean>>;
+  deleteTask: () => void;
 }
 
-export function Tasks({ taskName }: TaskProps) {
+export function Tasks({
+  taskName,
+  completedTask,
+  toggleTask,
+  deleteTask,
+}: TaskProps) {
+  function handleTask() {
+    toggleTask(!completedTask);
+  }
+
   return (
     <>
       {!taskName ? (
@@ -12,13 +25,17 @@ export function Tasks({ taskName }: TaskProps) {
       ) : (
         <section className="flex justify-between items-center my-2 gap-4 bg-zinc-700 p-2 rounded">
           <div className="flex items-center gap-2">
-            <button>
-              <BsCheckSquareFill className="h-5 w-5 fill-purple-400" />
+            <button onClick={handleTask}>
+              {completedTask ? (
+                <BsCheckSquareFill className="h-5 w-5 fill-purple-400" />
+              ) : (
+                <BsSquare className="h-5 w-5 fill-purple-400" />
+              )}
             </button>
             <span>{taskName}</span>
           </div>
 
-          <button>
+          <button onClick={deleteTask}>
             <BsFillTrash3Fill className="h-5 w-5 duration-200 transition-all fill-zinc-400 hover:fill-red-400" />
           </button>
         </section>
