5d30a24a7487895db5d5ac0f2049e2148623fc0e
Marc0sGabriel <mgabriel4747@gmail.com>
2023-11-02 08:40:56 -0300

    :sparkles: feat: connect database and fetch data

--- app/api/lofi/lofi.ts
+++ app/api/lofi/lofi.ts
@@ -1,23 +1,18 @@
-interface LofiData {
-  range: string;
-  majorDimension: string;
-  values: string[];
-}
+import { Database } from '@/types/database.types';
+import { createClient } from '@supabase/supabase-js';
 
-export async function fetchLofiSongs(): Promise<LofiData> {
-  const url = process.env.API_URL!;
+export const dynamic = 'force-dynamic';
 
-  const response = await fetch(url, {
-    method: 'GET',
-    headers: {
-      'content-type': 'application/json;charset=UTF-8',
-    },
-    next: {
-      revalidate: 72 * 3600, // revalidate each 3 days
-    },
-  }).then((r) => {
-    return r.json();
-  });
+export default async function fetchLofiSongs() {
+  const supabase = createClient<Database>(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!
+  );
 
-  return response;
+  const { data: songs } = await supabase
+    .from('songs')
+    .select('')
+    .order('title');
+
+  return songs;
 }
