13f72e18512022a12f99a17c6be9d8f4b1943e17
Marc0sGabriel <mgabriel4747@gmail.com>
2023-11-02 16:36:35 -0300

    :bug: fix: select all data from db

--- app/api/lofi/lofi.ts
+++ app/api/lofi/lofi.ts
@@ -11,7 +11,7 @@ export default async function fetchLofiSongs() {
 
   const { data: songs } = await supabase
     .from('songs')
-    .select('')
+    .select('*')
     .order('title');
 
   return songs;
