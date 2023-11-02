67589e946f13374a7cb9896c041d2bb51167e92e
Marc0sGabriel <mgabriel4747@gmail.com>
2023-11-02 08:43:30 -0300

    :sparkles: feat: get the data and pass to children

--- app/interface/components/MusicPlayer/Player/index.tsx
+++ app/interface/components/MusicPlayer/Player/index.tsx
@@ -1,14 +1,13 @@
-import { fetchLofiSongs } from '@/app/api/lofi/lofi';
+import fetchLofiSongs from '@/app/api/lofi/lofi';
+
 import { MusicPlayerComponent } from '..';
+
+export const dynamic = 'force-dynamic';
 
 export async function Player() {
   const data = await fetchLofiSongs();
 
-  function onlyFirst10Elements() {
-    return data.values.slice(0, 10);
-  }
+  const getSongsLinkOnly = data!.map((song) => song.song_link);
 
-  const getSongs = onlyFirst10Elements().map((songs) => songs[3]);
-
-  return <MusicPlayerComponent songs={getSongs} />;
+  return <MusicPlayerComponent data={data} songsLink={getSongsLinkOnly} />;
 }
