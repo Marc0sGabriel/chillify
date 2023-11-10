f2a8e16c4852d815ce9da75958a33885d5eb67c0
Marc0sGabriel <mgabriel4747@gmail.com>
2023-11-10 17:38:14 -0300

    feat: data fetch validation

--- app/api/lofi/lofi.ts
+++ app/api/lofi/lofi.ts
@@ -1,7 +1,7 @@
 import { Database } from '@/types/database.types';
 import { createClient } from '@supabase/supabase-js';
 
-export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 export default async function fetchLofiSongs() {
   const supabase = createClient<Database>(
@@ -9,10 +9,40 @@ export default async function fetchLofiSongs() {
     process.env.SUPABASE_ANON_KEY!
   );
 
-  const { data: songs } = await supabase
+  const { data: songs, error } = await supabase
     .from('songs')
     .select('*')
-    .order('title');
+    .order('song_link');
 
-  return songs;
+  if (error) {
+    // Lida com erros da consulta ao backend
+    console.error('Error fetching data:', error);
+    return null;
+  }
+
+  // Validação dos dados
+  if (songs && Array.isArray(songs)) {
+    const validatedSongs = songs.map((song) => {
+      const validatedSong = {
+        id: song.id,
+        title: song.title,
+        artist: song.artist,
+        song_cover: song.song_cover,
+        song_link: song.song_link,
+        created_at: song.created_at,
+      };
+
+      // Validação para o campo title
+      if (!validatedSong.title) {
+        console.error(`Field 'title' empty for song of ID ${validatedSong.id}`);
+      }
+
+      return validatedSong;
+    });
+
+    return validatedSongs;
+  } else {
+    console.error('Invalid data:', songs);
+    return null;
+  }
 }
